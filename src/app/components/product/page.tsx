6ccfbeee6740b3f69fa970ec7e11c4a430303436
iamanum <iamanummunir>
2025-02-04 19:30:59 +0500

    Fix ESLint unescaped entity issue

--- src/app/components/product/page.tsx
+++ src/app/components/product/page.tsx
@@ -67,7 +67,7 @@ const ProductCards: React.FC = () => {
 
     return (
         <div className="p-4">
-            <h2 className="text-center text-slate-800 mt-4 mb-4">Products from API's Data</h2>
+            <h2 className="text-center text-slate-800 mt-4 mb-4">Products from API&aposs Data</h2>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {products.map((product) => (
